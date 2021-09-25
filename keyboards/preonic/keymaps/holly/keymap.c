a4d113adf743f7ecbcb91650f62c3764a157827e
HoLLy <dario.db@hotmail.com>
2021-09-26 00:22:14 +0200

    Add navigation keys to adjust layer

--- keyboards/preonic/keymaps/holly/keymap.c
+++ keyboards/preonic/keymaps/holly/keymap.c
@@ -102,7 +102,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      | Reset| Debug|      |      |      |      |TermOf|TermOn|      |      |      |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |      | Clic |Aud cy|Aud on|AudOff|      |      |      |      |      |      |      |
+ * |      | Clic |Aud cy|Aud on|AudOff|      |      | Home | PgDn | PgUp | End  |      |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * |      |Voice-|Voice+|Mus on|MusOff|MidiOn|MidOff|      |      |      |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
@@ -112,7 +112,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_ADJUST] = LAYOUT_preonic_grid(
   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
   _______, RESET,   DEBUG,   _______, _______, _______, _______, TERM_ON, TERM_OFF,_______, _______, _______,
-  _______, CK_TOGG, MU_MOD,  AU_ON,   AU_OFF,  _______, _______, BASE,    _______, _______, _______, _______,
+  _______, CK_TOGG, MU_MOD,  AU_ON,   AU_OFF,  _______, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  _______,
   _______, MUV_DE,  MUV_IN,  MU_ON,   MU_OFF,  MI_ON,   MI_OFF,  _______, _______, _______, _______, _______,
   _______, _______, _______, _______, _______, _______, _______, _______, KC_RALT, KC_RGUI, KC_APP,  KC_RCTL
 )
--- keyboards/preonic/keymaps/holly/readme.md
+++ keyboards/preonic/keymaps/holly/readme.md
@@ -4,7 +4,7 @@ Changes from the default Preonic layout:
 - **Changed right space key to be shift**
 - **Reorder bottom left CTRL, SUPER, ALT keys** to follow standard ANSI layout
 - **Move symbol keys from raise to lower layer** to make pressing them with shift easier
-- **Add arrow keys to raised layer** on `jkl;`
+- **Add navigation keys to layer on `jkl;`**. This includes arrow keys on raise layer and HOME/PgDn/PgUp/END on adjust layer.
 - **Remove duplicated and strangely placed keys**, which originally came from the planck base
 - Removed dvorak and colemak layers
 - Add right control/alt/super/app keys on adjust layer
